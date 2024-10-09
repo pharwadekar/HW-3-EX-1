0f1ab50ccf2301854ef47e7f0c7217ef251fed83
pharwadekar <pharwadekar@tamu.edu>
2024-10-09 13:11:45 -0500

    Joins

--- answers.sh
+++ answers.sh
@@ -19,7 +19,7 @@ INSERT INTO graduates(Name, Age, Gender, Points) SELECT Name, Age, Gender, Point
 UPDATE graduates SET Graduation='08-09-2018' WHERE Name='Layal';
 DELETE FROM students WHERE Name='Layal';
 
-SELECT employees.name, employees.Company, companies.date FROM employees Inner join companies ON employees.Company=companies.name;
+SELECT employees.name, Company, date FROM employees Inner join companies ON employees.Company=companies.name;
 SELECT employees.name FROM employees Inner join companies ON employees.Company=companies.name and companies.Date < 2000;
 SELECT companies.name FROM companies Inner join employees ON employees.Company=companies.name and employees.Role='Graphic Designer';
 
